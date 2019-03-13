a0b9606d16ca8389b1ce8aa0533a4325a8b80cfc
igals <igals@DESKTOP-JS5MK5V>
2019-03-13 09:30:38 +0000

    Add Test Folder

--- src/main/java/smartspace/Application.java
+++ src/main/java/smartspace/Application.java
@@ -4,7 +4,7 @@ package main.java.smartspace;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Netef");
+		System.out.println("Added Test Folder");
 	}
 
 }
