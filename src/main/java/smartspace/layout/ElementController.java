acd9892cec446e269173dad41bb1a30ad35721c7
Tomer <tmriskl1@gmail.com>
2019-04-20 16:44:12 +0300

    Start ElementService

--- src/main/java/smartspace/layout/ElementController.java
+++ src/main/java/smartspace/layout/ElementController.java
@@ -2,7 +2,14 @@ package smartspace.layout;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import smartspace.logic.ElementService;
+
 @RestController
 public class ElementController {
+	private ElementService elementService;
+	
+	public ElementController(ElementService elementService) {
+		this.elementService = elementService;
+	}
 
 }
