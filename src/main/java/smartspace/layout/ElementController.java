b8613e97b447a62a4c19d91e930176cf1f0435e6
Tomer <tmriskl1@gmail.com>
2019-04-21 23:46:34 +0300

    Implementation for ElementService

--- src/main/java/smartspace/layout/ElementController.java
+++ src/main/java/smartspace/layout/ElementController.java
@@ -1,7 +1,6 @@
 package smartspace.layout;
 
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import smartspace.logic.ElementService;
 
 @RestController
