e9e59499af91abfca055e6aa22e603fd9aa81722
igals <igals@DESKTOP-JS5MK5V>
2019-05-08 15:34:33 +0100

    Fixed Eyal's notes

--- src/main/java/smartspace/logic/ActionServiceImpl.java
+++ src/main/java/smartspace/logic/ActionServiceImpl.java
@@ -12,38 +12,43 @@ import smartspace.dao.AdvancedElementDao;
 import smartspace.data.ActionEntity;
 import smartspace.data.ElementEntity;
 import smartspace.layout.ActionBoundary;
+
 @Service
 
-public class ActionServiceImpl implements ActionService{
-private AdvancedActionDao<String> ActionsDao;
-private AdvancedElementDao<String> ElementDao;
+public class ActionServiceImpl implements ActionService {
+	private AdvancedActionDao<String> ActionsDao;
+	private AdvancedElementDao<String> ElementDao;
 
-	
-	public ActionServiceImpl(AdvancedActionDao<String> ActionsDao,AdvancedElementDao<String> ElementDao) {
+	public ActionServiceImpl(AdvancedActionDao<String> ActionsDao, AdvancedElementDao<String> ElementDao) {
 		this.ActionsDao = ActionsDao;
-		this.ElementDao=ElementDao;
-		
+		this.ElementDao = ElementDao;
+
 	}
-	
-	
+
 	@Transactional
 	@Override
 	public List<ActionEntity> importActions(List<ActionEntity> actions) {
-			List<ActionEntity> actionEntites=new ArrayList<ActionEntity>();
-			List<ElementEntity>elements= ElementDao.readAll();
+		List<ActionEntity> actionEntites = new ArrayList<ActionEntity>();
+		List<ElementEntity> elements = this.ElementDao.readAll();
+		boolean foundElement=false;
 		for (ActionEntity action : actions) {
-			for (ElementEntity element:elements ) {
-				if (!element.getElementSmartspace().equals(action.getElementSmartspace())
-						&&!element.getElementId().equals(action.getElementId()))
-					throw new RuntimeException("You are trying to import actions from element who wasn't been imported");
+			for (ElementEntity element : elements) {
+				if (element.getElementSmartspace().equals(action.getElementSmartspace())) {
+					foundElement=true;
+					break;
+				}
+			}
+			if (!foundElement)
+				throw new RuntimeException("You are trying to import action with no element");
+			else {
+				if (action.getActionSmartspace().equals("inbal1"))
+					throw new RuntimeException("You are trying to import actions from your project");
+				else
+					actionEntites.add(this.ActionsDao.create(action));
 
 			}
-			if(action.getActionSmartspace().equals("inbala1"))
-					
-					throw new RuntimeException("You are trying to import actions from your own project-can't do that");
-			else actionEntites.add(this.ActionsDao.create(action));
 		}
-		return actionEntites;	
+		return actionEntites;
 	}
 
 	@Override
